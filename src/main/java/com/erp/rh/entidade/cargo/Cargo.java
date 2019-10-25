7457e0e5a5cbe960e16ee9e00e38c7a8e3b66cba
Raul de Paula <raul.p.p@hotmail.com>
2019-10-25 15:32:25 -0300

    Criei as entidades (Funcionario, departamento e cargo) sem adicionar as anotações do SpringBoot

--- src/main/java/com/erp/rh/entidade/cargo/Cargo.java
+++ src/main/java/com/erp/rh/entidade/cargo/Cargo.java
@@ -5,10 +5,25 @@
  */
 package com.erp.rh.entidade.cargo;
 
+import com.erp.rh.entidade.departamento.Departamento;
+import java.time.LocalDate;
+
 /**
  *
  * @author Raul Portela
  */
 public class Cargo {
+
+    private Long id;
+
+    private String nome;
+
+    private Departamento departamento;
+
+    private LocalDate dtInicio;
+
+    private LocalDate dtFim;
+
+    private Float Salario ;
     
 }
